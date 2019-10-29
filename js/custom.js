f71f2b6b42cf056f9f2d6e8c5a6deedd5d886b8d
Tomori <291340703@qq.com>
2019-10-29 17:25:46 +0800

    update

--- js/custom.js
+++ js/custom.js
@@ -6,20 +6,20 @@ var mapLoaded = false,
     map;
 /*---------------Baidu Maps Scripts-------------------------*/
 //创建和初始化地图函数：
-function initMap(){
-    createMap();//创建地图
-}
+// function initMap(){
+//     createMap();//创建地图
+// }
 
-//创建地图函数：
-function createMap(){
-    var map = new BMap.Map("Map");//在百度地图容器中创建一个地图
-    var point = new BMap.Point(113.3406,23.1096);//定义一个中心点坐标
-    map.centerAndZoom(point,18);//设定地图的中心点和坐标并将地图显示在地图容器中
-    window.map = map;//将map变量存储在全局
-}
+// //创建地图函数：
+// function createMap(){
+//     var map = new BMap.Map("Map");//在百度地图容器中创建一个地图
+//     var point = new BMap.Point(113.3406,23.1096);//定义一个中心点坐标
+//     map.centerAndZoom(point,18);//设定地图的中心点和坐标并将地图显示在地图容器中
+//     window.map = map;//将map变量存储在全局
+// }
 
-//创建和初始化地图
-initMap();
+// //创建和初始化地图
+// initMap();
 
 /*---------------Google Maps Scripts-------------------------*/
 // function initMap() {
